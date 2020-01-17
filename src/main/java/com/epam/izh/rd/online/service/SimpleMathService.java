db5c3953626eb961113fbdc5337442ab65f86c4b
Anatoly Sochnev <starkdavk@gmail.com>
2020-01-18 00:58:43 +0400

    Добавил действия в метод calcFibonacci.

--- src/main/java/com/epam/izh/rd/online/service/SimpleMathService.java
+++ src/main/java/com/epam/izh/rd/online/service/SimpleMathService.java
@@ -103,7 +103,21 @@ public class SimpleMathService implements MathService {
      */
     @Override
     public long calcFibonacci(int number) {
-        return -1L;
+        long[] numberFibonacci = new long[number + 1];
+
+        if (number == 0) {
+            return 0;
+        } else if (number == 1 || number == 2) {
+            return 1;
+        }
+
+        for (int i = 3; i <= number; i++) {
+            numberFibonacci[0] = 0;
+            numberFibonacci[1] = 1;
+            numberFibonacci[2] = 1;
+            numberFibonacci[i] = numberFibonacci[i - 2] + numberFibonacci[i -1];
+        }
+        return numberFibonacci[number];
     }
 
     /**
